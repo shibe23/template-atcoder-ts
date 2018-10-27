4a96f82833939aa550cf18079308f485377b2263
shibe23 <shibe23.info@gmail.com>
2018-10-28 00:04:34 +0900

    first commit.

---
+++ build/main.js
@@ -0,0 +1,4 @@
+// node_modules/.bin/ts-node src/main.ts
+// node_modules/.bin/tsc
+var test = "Hello";
+console.log(test);
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+// node_modules/.bin/ts-node src/main.ts
+// node_modules/.bin/tsc
+
+let test = "Hello"
+
+console.log(test)
